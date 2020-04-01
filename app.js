f5ad697aaf957625e5628f0891b62ccad158d3bd
Brian Marquis <rbmarquis.dev@gmail.com>
2020-03-31 23:59:27 -0500

    add schema & resolvers

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
 const express = require('express');
+const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res, next) => {
-  res.send('Hello World!');
-});
+app.use('/graphql', graphqlHttp({
+  schema: buildSchema(`
+    type RootQuery {
+      events: [String!]!
+    }
+    type RootMutation {
+      createEvent(name: String): String
+    }
+    schema {
+      query: RootQuery
+      mutation: RootMutation
+    }
+  `),
+  rootValue: {
+    events: () => {
+      return ['Romantic Cooking', 'Sailing', 'All-night Coding'];
+    },
+    createEvent: (args) => {
+      const eventName = args.name;
+      return eventName;
+    }
+  },
+  graphiql: true
+}));
 
 app.listen(3000);
