be4fafc03def3ab60c05a5df3c8c8509af15f7e3
Brian Marquis <rbmarquis.dev@gmail.com>
2020-04-02 20:34:53 -0500

    add createUser resolver, add user to createEvent resolver

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const graphqlHttp = require('express-graphql');
 const { buildSchema } = require('graphql');
 const mongoose = require('mongoose');
 const { PORT, MONGODB_URI } = process.env;
+const bcrypt = require('bcryptjs');
 
 const Event = require('./models/event');
+const User = require('./models/user');
 
 const app = express();
 
@@ -22,11 +24,24 @@ app.use('/graphql', graphqlHttp({
       date: String!
     }
 
+    type User {
+      _id: ID!
+      email: String!
+      # password is nullable here - don't want to retrieve it in queries
+      password: String
+    }
+
     input EventInput {
       title: String!
       description: String!
       price: Float!
       date: String!
+    }
+
+    input UserInput {
+      email: String!
+      # Must require a password here
+      password: String!
     }
 
     type RootQuery {
@@ -35,6 +50,7 @@ app.use('/graphql', graphqlHttp({
 
     type RootMutation {
       createEvent(eventInput: EventInput): Event
+      createUser(userInput: UserInput): User
     }
 
     schema {
@@ -44,6 +60,7 @@ app.use('/graphql', graphqlHttp({
   `),
   rootValue: {
     events: () => {
+      // Always `return` if async so graphql knows to wait for promise to resolve
       return Event.find().then(events => {
         return events.map(event => {
           return {...event._doc};
@@ -60,22 +77,57 @@ app.use('/graphql', graphqlHttp({
         title: args.eventInput.title,
         description: args.eventInput.description,
         price: +args.eventInput.price,
-        date: new Date(args.eventInput.date)
+        date: new Date(args.eventInput.date),
+        creator: '5e868aab8197c91a08815bfa'
       });
+      let createdEvent;
+      // Always `return` if async so graphql knows to wait for promise to resolve
       return event
         .save()
         .then(result => {
-          console.log(result);
-          return {...result._doc};
-          // If the _id field throws an arror, override the original by converting it to a string:
-          // return {...event._doc, _id: event._doc._id.toString() };
-          // Or, use the id field added by mongoose:
-          // return {...event._doc, _id: event.id };
+          createdEvent = {...result._doc, _id: result.id};
+          return User.findById('5e868aab8197c91a08815bfa');
+        })
+        .then(user => {
+          if (!user) {
+            throw new Error('User not found.');
+          }
+          user.createdEvents.push(event);
+          return user.save();
+        })
+        .then(result => {
+          return createdEvent;
         })
         .catch(err => {
           console.log(err);
           throw err;
         });
+    },
+    createUser: (args) => {
+      // Check if user exists, otherwise, create user
+      // Always `return` if async so graphql knows to wait for promise to resolve
+      return User.findOne({ email: args.userInput.email })
+        .then(user => {
+          if (user) {
+            throw new Error('User exists already.');
+          }
+          return bcrypt.hash(args.userInput.password, 12);
+        })
+        .then(hashedPassword => {
+          const user = new User({
+            email: args.userInput.email,
+            password: hashedPassword
+          });
+          return user.save();
+        })
+        .then(result => {
+          // After saving user, nullify the returned password field
+          return { ...result._doc, password: null, _id: result.id };
+        })
+        .catch(err => {
+          throw err;
+        });
+      
     }
   },
   graphiql: true
