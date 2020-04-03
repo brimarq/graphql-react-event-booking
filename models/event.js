995482756cdbda0caaa2ad7573d03e6dbd01a1b7
Brian Marquis <rbmarquis.dev@gmail.com>
2020-04-02 20:32:36 -0500

    add creator field to ref user

--- models/event.js
+++ models/event.js
@@ -18,6 +18,11 @@ const eventSchema = new Schema({
   date: {
     type: Date,
     required: true
+  },
+  // Define relationship of event to the user who created it, referencing User object id
+  creator: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
   }
 });
 
