2e13853df3b7d1f81db5da6cf48eb0ea656482db
Brian Marquis <rbmarquis.dev@gmail.com>
2020-04-02 23:18:03 -0500

    create schema module

---
+++ graphql/schema/index.js
@@ -0,0 +1,47 @@
+const { buildSchema } = require('graphql');
+
+module.exports = buildSchema(`
+    type Event {
+      _id: ID!
+      title: String!
+      description: String!
+      price: Float!
+      date: String!
+      creator: User!
+    }
+
+    type User {
+      _id: ID!
+      email: String!
+      # password is nullable here - don't want to retrieve it in queries
+      password: String
+      createdEvents: [Event!]
+    }
+
+    input EventInput {
+      title: String!
+      description: String!
+      price: Float!
+      date: String!
+    }
+
+    input UserInput {
+      email: String!
+      # Must require a password here
+      password: String!
+    }
+
+    type RootQuery {
+      events: [Event!]!
+    }
+
+    type RootMutation {
+      createEvent(eventInput: EventInput): Event
+      createUser(userInput: UserInput): User
+    }
+
+    schema {
+      query: RootQuery
+      mutation: RootMutation
+    }
+  `)
