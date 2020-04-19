5c4761441c797aaa84d494fde12a00793f34f763
Brian Marquis <rbmarquis.dev@gmail.com>
2020-04-18 22:50:22 -0500

    add booking schema

--- graphql/schema/index.js
+++ graphql/schema/index.js
@@ -1,47 +1,58 @@
 const { buildSchema } = require('graphql');
 
 module.exports = buildSchema(`
-    type Event {
-      _id: ID!
-      title: String!
-      description: String!
-      price: Float!
-      date: String!
-      creator: User!
-    }
+  type Booking {
+    _id: ID!
+    event: Event!
+    user: User!
+    createdAt: String!
+    updatedAt: String!
+  }
 
-    type User {
-      _id: ID!
-      email: String!
-      # password is nullable here - don't want to retrieve it in queries
-      password: String
-      createdEvents: [Event!]
-    }
+  type Event {
+    _id: ID!
+    title: String!
+    description: String!
+    price: Float!
+    date: String!
+    creator: User!
+  }
 
-    input EventInput {
-      title: String!
-      description: String!
-      price: Float!
-      date: String!
-    }
+  type User {
+    _id: ID!
+    email: String!
+    # password is nullable here - don't want to retrieve it in queries
+    password: String
+    createdEvents: [Event!]
+  }
 
-    input UserInput {
-      email: String!
-      # Must require a password here
-      password: String!
-    }
+  input EventInput {
+    title: String!
+    description: String!
+    price: Float!
+    date: String!
+  }
 
-    type RootQuery {
-      events: [Event!]!
-    }
+  input UserInput {
+    email: String!
+    # Must require a password here
+    password: String!
+  }
 
-    type RootMutation {
-      createEvent(eventInput: EventInput): Event
-      createUser(userInput: UserInput): User
-    }
+  type RootQuery {
+    events: [Event!]!
+    bookings: [Booking!]
+  }
 
-    schema {
-      query: RootQuery
-      mutation: RootMutation
-    }
-  `)
+  type RootMutation {
+    createEvent(eventInput: EventInput): Event
+    createUser(userInput: UserInput): User
+    bookEvent(eventId: ID!) : Booking!
+    cancelBooking(bookingId: ID!): Event!
+  }
+
+  schema {
+    query: RootQuery
+    mutation: RootMutation
+  }
+`)
