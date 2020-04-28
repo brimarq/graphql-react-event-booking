b258c5b66b50dfeb71adbf040bf6569067ff092b
Brian Marquis <rbmarquis.dev@gmail.com>
2020-04-28 17:53:48 -0500

    add AuthData type, login to RootQuery

--- graphql/schema/index.js
+++ graphql/schema/index.js
@@ -26,6 +26,12 @@ module.exports = buildSchema(`
     createdEvents: [Event!]
   }
 
+  type AuthData {
+    userId: ID!
+    token: String!
+    tokenExpiration: Int!
+  }
+
   input EventInput {
     title: String!
     description: String!
@@ -41,7 +47,8 @@ module.exports = buildSchema(`
 
   type RootQuery {
     events: [Event!]!
-    bookings: [Booking!]
+    bookings: [Booking!]!
+    login(email: String!, password: String!): AuthData!
   }
 
   type RootMutation {
