31cb66988a2d4a6013a8ef5e7087d2178c357fe8
Brian Marquis <rbmarquis.dev@gmail.com>
2020-04-29 01:19:44 -0500

    add err check & return data to console

--- frontend/src/pages/Auth.js
+++ frontend/src/pages/Auth.js
@@ -36,6 +36,18 @@ class AuthPage extends Component {
       headers: {
         'Content-Type': 'application/json'
       }
+    })
+    .then(res => {
+      if(res.status !== 200 & res.status !== 201) {
+        throw new Error('Failed!');
+      }
+      return res.json();
+    })
+    .then(resData => {
+      console.log(resData);
+    })
+    .catch(err => {
+      console.log(err);
     });
   };
 
