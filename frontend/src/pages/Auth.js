467a9dcfb1d7336bc1577053d44e441bf201cc45
Brian Marquis <rbmarquis.dev@gmail.com>
2020-04-29 01:14:13 -0500

    connect form to api

--- frontend/src/pages/Auth.js
+++ frontend/src/pages/Auth.js
@@ -2,16 +2,53 @@ import React, { Component } from 'react';
 import './Auth.css';
 
 class AuthPage extends Component {
+  constructor(props) {
+    super(props);
+    this.emailEl = React.createRef();
+    this.passwordEl = React.createRef();
+
+  }
+
+  submitHandler = event => {
+    event.preventDefault();
+    const email = this.emailEl.current.value;
+    const password = this.passwordEl.current.value;
+
+    if(email.trim().length === 0 || password.trim().length === 0) {
+      return;
+    }
+
+    const requestBody = {
+      query: `
+        mutation {
+          createUser(userInput: {email: "${email}", password: "${password}"}) {
+            _id
+            email
+          }
+        }
+      `
+
+    };
+    
+    fetch('http://localhost:8000/graphql', {
+      method: 'POST',
+      body: JSON.stringify(requestBody), 
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+  };
+
   render() {
     return (
-      <form className="auth-form" action="">
+      <form className="auth-form" onSubmit={this.submitHandler}>
         <div className="form-control">
           <label htmlFor="email">Email</label>
-          <input type="email" id="email"/>
+          <input type="email" id="email" ref={this.emailEl}/>
         </div>
         <div className="form-control">
           <label htmlFor="password">Password</label>
-          <input type="password" id="password"/>
+          <input type="password" id="password" ref={this.passwordEl}/>
         </div>
         <div className="form-actions">
           <button type="submit">Submit</button>
