20b9aadf660baa27d8821eccacc30da5e6cd2216
Brian Marquis <rbmarquis.dev@gmail.com>
2020-04-28 23:57:37 -0500

    create auth form

--- frontend/src/pages/Auth.js
+++ frontend/src/pages/Auth.js
@@ -1,8 +1,24 @@
 import React, { Component } from 'react';
+import './Auth.css';
 
 class AuthPage extends Component {
   render() {
-    return <h1>The Auth Page</h1>;
+    return (
+      <form className="auth-form" action="">
+        <div className="form-control">
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email"/>
+        </div>
+        <div className="form-control">
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password"/>
+        </div>
+        <div className="form-actions">
+          <button type="submit">Submit</button>
+          <button type="button">Switch to Signup</button>
+        </div>
+      </form>
+    );
   }
 }
 
