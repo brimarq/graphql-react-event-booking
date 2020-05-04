4c8e11ea362c5dad1c49d4b395f0b094d52eebcd
Brian Marquis <rbmarquis.dev@gmail.com>
2020-05-03 21:06:48 -0500

    add AuthContext, conditional render of NavLinks

--- frontend/src/components/Navigation/MainNavigation.js
+++ frontend/src/components/Navigation/MainNavigation.js
@@ -1,20 +1,33 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import AuthContext from '../../context/auth-context';
 import './MainNavigation.css';
 
 const mainNavigation = props => (
-  <header className="main-navigation">
-    <div className="main-navigation__logo">
-      <h1>Easy Event</h1>
-    </div>
-    <nav className="main-navigation__items">
-      <ul>
-        <li><NavLink to="/auth">Authenticate</NavLink></li>
-        <li><NavLink to="/events">Events</NavLink></li>
-        <li><NavLink to="/bookings">Bookings</NavLink></li>
-      </ul>
-    </nav>
-  </header>
+  <AuthContext.Consumer>
+    {(context) => {
+      return (
+        <header className="main-navigation">
+          <div className="main-navigation__logo">
+            <h1>Easy Event</h1>
+          </div>
+          <nav className="main-navigation__items">
+            <ul>
+              {/* Only render if unauthenticated (no token) */}
+              {!context.token && (
+                <li><NavLink to="/auth">Authenticate</NavLink></li>
+              )}
+              <li><NavLink to="/events">Events</NavLink></li>
+              {/* Only render if authenticated (has token)  */}
+              {context.token && (
+                <li><NavLink to="/bookings">Bookings</NavLink></li>
+              )}
+            </ul>
+          </nav>
+        </header>
+      );
+    }}
+  </AuthContext.Consumer>
 );
 
 export default mainNavigation;
