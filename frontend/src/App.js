ff90ea0b073417be564696dee4cf75474883464c
Brian Marquis <rbmarquis.dev@gmail.com>
2020-05-05 17:35:51 -0500

    refactor routes & redirects

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -38,15 +38,17 @@ class App extends Component {
             <MainNavigation />
             <main className="main-content">
               <Switch>
-                {/* Only redirect/allow auth page if there's no token */}
-                {!this.state.token && <Redirect from="/" to="/auth" exact />}
-                {!this.state.token && <Route path="/auth" component={AuthPage} />}
-                {/* Always allow events page */}
-                <Route path="/events" component={EventsPage} />
-                {/* Allow only if token is set */}
+                {/* Token redirects */}
                 {this.state.token && <Redirect from="/" to="/events" exact />}
                 {this.state.token && <Redirect from="/auth" to="/events" exact />}
+
+                {/* Routes */}
+                {!this.state.token && <Route path="/auth" component={AuthPage} />}
+                <Route path="/events" component={EventsPage} />
                 {this.state.token && <Route path="/bookings" component={BookingsPage} />}
+
+                {/* No token redirect */}
+                {!this.state.token && <Redirect to="/auth" exact />}
               </Switch>
             </main>
           </AuthContext.Provider>
