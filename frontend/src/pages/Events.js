cb297683d871837060f06c6f921111e3b42eabab
Brian Marquis <rbmarquis.dev@gmail.com>
2020-05-05 20:18:01 -0500

    add auth'd graphql req to event form, hide create event from unauth'd

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './Events.css';
 import Modal from '../components/Modal/Modal';
 import Backdrop from '../components/Backdrop/Backdrop';
+import AuthContext from '../context/auth-context';
 
 
 class EventsPage extends Component {
@@ -9,13 +10,94 @@ class EventsPage extends Component {
     creating: false
   };
 
+  static contextType = AuthContext;
+
+  constructor(props) {
+    super(props);
+    this.titleElRef = React.createRef();
+    this.priceElRef = React.createRef();
+    this.dateElRef = React.createRef();
+    this.descriptionElRef = React.createRef();
+  }
+
   startCreateEventHandler = () => {
     this.setState({ creating: true });
   }
 
   modalConfirmHandler = () => {
     this.setState({ creating: false });
+    const title = this.titleElRef.current.value;
+    const price = this.priceElRef.current.value;
+    const date = this.dateElRef.current.value;
+    const description = this.descriptionElRef.current.value;
+
+    if (
+      title.trim().length === 0 || 
+      price.trim().length === 0 || 
+      date.trim().length === 0 || 
+      description.trim().length === 0  
+    ) {
+      return;
+    }
+
+    const event = { title, price: +price, date, description };
+    console.log(event);
+
+    const requestBody = {
+      query: `
+        mutation {
+          createEvent(eventInput: { 
+            title: "${title}", 
+            description: "${description}",
+            price: ${+price},
+            date: "${date}"
+          }) {
+            _id
+            title
+            description
+            date
+            price
+            creator {
+              _id
+              email
+            }
+          }
+        }
+      `
+    };
+
+    const token = this.context.token;
+
+    fetch('http://localhost:8000/graphql', {
+      method: 'POST',
+      body: JSON.stringify(requestBody), 
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      }
+    })
+    .then(res => {
+      if(res.status !== 200 & res.status !== 201) {
+        throw new Error('Failed!');
+      }
+      return res.json();
+    })
+    .then(resData => {
+      // Set context with resData info if token is present
+      // if (resData.data.login.token) {
+      //   this.context.login(
+      //     resData.data.login.token,
+      //     resData.data.login.userId,
+      //     resData.data.login.tokenExpiration
+      //   );
+      // }
+      console.log(resData);
+    })
+    .catch(err => {
+      console.log(err);
+    });
   };
+  
 
   modalCancelHandler = () => {
     this.setState({ creating: false });
@@ -33,13 +115,32 @@ class EventsPage extends Component {
             onCancel={this.modalCancelHandler} 
             onConfirm={this.modalConfirmHandler}
           >
-            <p>Modal Content</p>
+            <form action="">
+              <div className="form-control">
+                <label htmlFor="title">Title</label>
+                <input type="text" id="title" ref={this.titleElRef}/>
+              </div>
+              <div className="form-control">
+                <label htmlFor="price">Price</label>
+                <input type="number" id="price" ref={this.priceElRef}/>
+              </div>
+              <div className="form-control">
+                <label htmlFor="date">Date</label>
+                <input type="datetime-local" id="date" ref={this.dateElRef}/>
+              </div>
+              <div className="form-control">
+                <label htmlFor="description">Description</label>
+                <textarea name="" id="description" rows="4" ref={this.descriptionElRef}></textarea>
+              </div>
+            </form>
           </Modal>
         )}
-        <div className="events-control">
-          <p>Share your own Events!</p>
-          <button className="btn" onClick={this.startCreateEventHandler}>Create Event</button>
-        </div>
+        {this.context.token && (
+          <div className="events-control">
+            <p>Share your own Events!</p>
+            <button className="btn" onClick={this.startCreateEventHandler}>Create Event</button>
+          </div>
+        )}
       </>
       
     );
