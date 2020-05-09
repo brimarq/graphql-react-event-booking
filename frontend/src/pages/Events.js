173d7ebb706ff37bd1972e6929636454869f1279
Brian Marquis <rbmarquis.dev@gmail.com>
2020-05-09 17:39:26 -0500

    add bookEventHandler

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -163,7 +163,47 @@ class EventsPage extends Component {
     });
   }
 
-  bookEventHandler = () => {}
+  bookEventHandler = () => {
+    if (!this.context.token) {
+      this.setState({ selectedEvent: null });
+      return;
+    }
+
+    const requestBody = {
+      query: `
+        mutation {
+          bookEvent(eventId: "${this.state.selectedEvent._id}") {
+            _id
+            createdAt
+            updatedAt
+          }
+        }
+      `
+    };
+
+    fetch('http://localhost:8000/graphql', {
+      method: 'POST',
+      body: JSON.stringify(requestBody), 
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${this.context.token}`
+      }
+    })
+    .then(res => {
+      if(res.status !== 200 & res.status !== 201) {
+        throw new Error('Failed!');
+      }
+      return res.json();
+    })
+    .then(resData => {
+      console.log(resData);
+      this.setState({ selectedEvent: null });
+    })
+    .catch(err => {
+      console.log(err);
+      // this.setState({ isLoading: false });
+    });
+  }
   
 
   render() {
@@ -208,7 +248,7 @@ class EventsPage extends Component {
             canConfirm 
             onCancel={this.modalCancelHandler} 
             onConfirm={this.bookEventHandler}
-            confirmText="Book"
+            confirmText={this.context.token ? 'Book' : 'Confirm'}
           >
             <h1>{this.state.selectedEvent.title}</h1>
             <h2>${this.state.selectedEvent.price} - {new Date(this.state.selectedEvent.date).toLocaleDateString()}</h2>
