a316804b2f4f8dca3fa53616c8d8daf04f376787
Brian Marquis <rbmarquis.dev@gmail.com>
2020-05-09 21:49:17 -0500

    correct graphql query variables

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -56,12 +56,12 @@ class EventsPage extends Component {
 
     const requestBody = {
       query: `
-        mutation {
+        mutation CreateEvent(($title: String!, $desc: String!, $price: Float!, $date: String!)) {
           createEvent(eventInput: { 
-            title: "${title}", 
-            description: "${description}",
-            price: ${+price},
-            date: "${date}"
+            title: $title, 
+            description: $desc,
+            price: $price,
+            date: $date
           }) {
             _id
             title
@@ -70,17 +70,21 @@ class EventsPage extends Component {
             price
           }
         }
-      `
+      `,
+      variables: {
+        title: title, 
+        desc: description,
+        price: +price,
+        date: date
+      }
     };
-
-    const token = this.context.token;
 
     fetch('http://localhost:8000/graphql', {
       method: 'POST',
       body: JSON.stringify(requestBody), 
       headers: {
         'Content-Type': 'application/json',
-        'Authorization': `Bearer ${token}`
+        'Authorization': `Bearer ${this.context.token}`
       }
     })
     .then(res => {
@@ -179,14 +183,17 @@ class EventsPage extends Component {
 
     const requestBody = {
       query: `
-        mutation {
-          bookEvent(eventId: "${this.state.selectedEvent._id}") {
+        mutation BookEvent($id: ID!) {
+          bookEvent(eventId: $id) {
             _id
             createdAt
             updatedAt
           }
         }
-      `
+      `,
+      variables: {
+        id: this.state.selectedEvent._id
+      }
     };
 
     fetch('http://localhost:8000/graphql', {
