ad55ebd43f096d5d921f20acb966990f97fed5e3
Brian Marquis <rbmarquis.dev@gmail.com>
2020-05-09 19:35:12 -0500

    bind cancel button onClick func

--- frontend/src/components/Bookings/BookingList/BookingList.js
+++ frontend/src/components/Bookings/BookingList/BookingList.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import BookingItem from './BookingItem/BookingItem';
 import './BookingList.css';
 
 const bookingList = props => (
@@ -12,9 +11,7 @@ const bookingList = props => (
         <div className="bookings__item-actions">
           <button className="btn" onClick={props.onDelete.bind(this, booking._id)}>Cancel</button>
         </div>
-        
       </li>
-      // <BookingItem booking={booking}/>
     ))}
   </ul>
 );
